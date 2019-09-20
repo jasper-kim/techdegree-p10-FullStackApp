d1e92f41df40098ac576312ff1ce1bb89f91c573
Jasper Kim <yjikim0219@gmail.com>
2019-09-19 18:23:04 -0700

    Updated style
    Added shadow to Course.js
    Changed colors through the app

--- client/src/Components/Courses.js
+++ client/src/Components/Courses.js
@@ -31,7 +31,7 @@ export default class Courses extends Component {
                 {/* Loop courses state and return HTML elment */
                     this.state.courses.map(course => {return (
                         <div key={course.id} className="grid-33">
-                            <Link className="course--module course--link" to={`/courses/${course.id}`}>
+                            <Link className="course--module course--link shadow" to={`/courses/${course.id}`}>
                                 <h4 className="course--label">Course</h4>
                                 <h3 className="course--title">{course.title}</h3>
                             </Link>
@@ -39,7 +39,7 @@ export default class Courses extends Component {
                 )}
 
                 <div className="grid-33">
-                    <Link className="course--module course--add--module" to="/courses/create">
+                    <Link className="course--module course--add--module shadow" to="/courses/create">
                         <h3 className="course--add--title">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
                                 <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
